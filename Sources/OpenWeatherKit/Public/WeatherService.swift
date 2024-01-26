c7f7420fac8d7eef8041c39d9db6ba79302fa984
Jeremy Greenwood <jagreenwood@gmail.com>
2024-01-26 09:22:57 -0500

    Update `async-http-client` Package (#31)

--- Sources/OpenWeatherKit/Public/WeatherService.swift
+++ Sources/OpenWeatherKit/Public/WeatherService.swift
@@ -34,18 +34,16 @@ final public class WeatherService: Sendable {
         public var jwt: @Sendable () -> String
         public var language: Language
 #if os(Linux)
-        public var eventLoopGroupProvider: NIOEventLoopGroupProvider
-
+        @available(*, deprecated, renamed: "init(jwt:language:)")
         public init(
             jwt: @escaping @Sendable () -> String,
             language: WeatherService.Configuration.Language = .englishUS,
-            eventLoopGroupProvider: NIOEventLoopGroupProvider = .createNew
+            eventLoopGroupProvider: NIOEventLoopGroupProvider
         ) {
             self.jwt = jwt
             self.language = language
-            self.eventLoopGroupProvider = eventLoopGroupProvider
         }
-#else
+#endif
         /// Initializes an instance of Configuation
         /// - Parameters:
         ///   - jwt: A closure to provide a JWT.
@@ -57,7 +55,6 @@ final public class WeatherService: Sendable {
             self.jwt = jwt
             self.language = language
         }
-#endif
     }
 
     @usableFromInline
@@ -94,9 +91,7 @@ final public class WeatherService: Sendable {
     public init(configuration: Configuration) {
         Self.configuration = configuration
         self.networkClient = NetworkClient(
-            client: HTTPClient(
-                eventLoopGroupProvider: configuration.eventLoopGroupProvider.eventLoopGroupProvider
-            )
+            client: HTTPClient(eventLoopGroupProvider: .singleton)
         )
     }
 
