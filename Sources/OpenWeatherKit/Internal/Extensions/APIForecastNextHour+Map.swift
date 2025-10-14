20ae94a72429ec28347ef895d9c3a756d8bde7ac
Jeremy Greenwood <jagreenwood@gmail.com>
2025-10-14 16:24:57 -0400

    update nexthour/minute

--- Sources/OpenWeatherKit/Internal/Extensions/APIForecastNextHour+Map.swift
+++ Sources/OpenWeatherKit/Internal/Extensions/APIForecastNextHour+Map.swift
@@ -1,6 +1,6 @@
 //
 //  APIForecastNextHour+Map.swift
-//  
+//
 //
 //  Created by Jeremy Greenwood on 10/25/22.
 //
@@ -10,17 +10,19 @@ import Foundation
 extension APIForecastNextHour {
     var minuteForecast: Forecast<MinuteWeather> {
         Forecast<MinuteWeather>(
-            forecast: minutes.map(\.minuteWeather),
+            forecast: minutes.map { minute in
+                minute.minuteWeather(condition.first(where: { $0.startTime == minute.startTime }))
+            },
             metadata: metadata.weatherMetadata
         )
     }
 }
 
 extension APIMinute {
-    var minuteWeather: MinuteWeather {
+    func minuteWeather(_ condition: APICondition?) -> MinuteWeather {
         MinuteWeather(
             date: startTime,
-            precipitation: condition != nil ? (Precipitation(rawValue: condition!) ?? .none) : .none,
+            precipitation: condition != nil ? (Precipitation(rawValue: condition!.beginCondition) ?? .none) : .none,
             precipitationChance: precipitationChance,
             precipitationIntensity: Measurement(value: precipitationIntensity, unit: .metersPerSecond)
         )
--- Sources/OpenWeatherKit/Internal/Models/APIForecastNextHour.swift
+++ Sources/OpenWeatherKit/Internal/Models/APIForecastNextHour.swift
@@ -1,6 +1,6 @@
 //
 //  APIForecastNextHour.swift
-//  
+//
 //
 //  Created by Jeremy Greenwood on 10/16/22.
 //
@@ -9,34 +9,40 @@ import Foundation
 
 // MARK: - APIForecastNextHour
 struct APIForecastNextHour: Codable, Equatable {
-    let name: String
-    let metadata: APIMetadata
-    let summary: [APIMinute]
-    let forecastStart: Date
+    let condition: [APICondition]
     let forecastEnd: Date
+    let forecastStart: Date
+    let metadata: APIMetadata
     let minutes: [APIMinute]
+    let summary: [APIMinute]
 
     enum CodingKeys: String, CodingKey {
-        case name = "name"
-        case metadata = "metadata"
-        case summary = "summary"
-        case forecastStart = "forecastStart"
+        case condition = "condition"
         case forecastEnd = "forecastEnd"
+        case forecastStart = "forecastStart"
+        case metadata = "metadata"
         case minutes = "minutes"
+        case summary = "summary"
     }
 }
 
 // MARK: - APIMinute
 struct APIMinute: Codable, Equatable {
-    let startTime: Date
     let precipitationChance: Double
     let precipitationIntensity: Double
-    let condition: String?
+    let startTime: Date
 
     enum CodingKeys: String, CodingKey {
-        case startTime = "startTime"
         case precipitationChance = "precipitationChance"
         case precipitationIntensity = "precipitationIntensity"
-        case condition = "condition"
+        case startTime = "startTime"
     }
+}
+
+// MARK: - APICondition
+struct APICondition: Codable, Equatable {
+    @TextCaseCoding<Lowercased> var beginCondition: String
+    @TextCaseCoding<Lowercased> var endCondition: String
+    @TextCaseCoding<Lowercased> var forecastToken: String
+    let startTime: Date
 }
