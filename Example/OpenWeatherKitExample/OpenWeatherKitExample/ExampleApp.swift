1f039a3aa4e47787c61c344cc650e5220062b985
Jeremy Greenwood <jagreenwood@gmail.com>
2025-11-04 13:45:05 -0500

    init example app

---
+++ Example/OpenWeatherKitExample/OpenWeatherKitExample/ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenWeatherKitExampleApp.swift
+//  OpenWeatherKitExample
+//
+//  Created by Jeremy Greenwood on 11/4/25.
+//
+
+import SwiftUI
+
+@main
+struct ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
