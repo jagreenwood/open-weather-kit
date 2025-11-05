752a32e350cb85d2f0a8b3fa2c9ea44f803de9eb
Jeremy Greenwood <jagreenwood@gmail.com>
2025-11-05 15:10:55 -0500

    remove public from TextCaseCoding

--- Sources/OpenWeatherKit/Internal/Models/TextCaseCoding.swift
+++ Sources/OpenWeatherKit/Internal/Models/TextCaseCoding.swift
@@ -8,21 +8,21 @@
 import Foundation
 
 @propertyWrapper
-public struct TextCaseCoding<Case: TextCase>: Codable, Sendable {
-    public var wrappedValue: Case.Value
+struct TextCaseCoding<Case: TextCase>: Codable, Sendable {
+    var wrappedValue: Case.Value
 
-    public init(wrappedValue: Case.Value) {
+    init(wrappedValue: Case.Value) {
         self.wrappedValue = wrappedValue
     }
 
-    public func encode(to encoder: Encoder) throws {
+    func encode(to encoder: Encoder) throws {
         var container = encoder.singleValueContainer()
         try container.encode(Case.transform(wrappedValue))
     }
 }
 
 // swiftlint:disable force_cast
-public extension KeyedDecodingContainer {
+extension KeyedDecodingContainer {
     func decode<Case: TextCase>(_ type: TextCaseCoding<Case>.Type, forKey key: Key) throws -> TextCaseCoding<Case> {
         if Case.Value.self is Optional<String>.Type {
             try TextCaseCoding(wrappedValue: Case.transform(decodeIfPresent(String.self, forKey: key) as! Case.Value))
@@ -40,44 +40,44 @@ public extension KeyedDecodingContainer {
 extension TextCaseCoding: Equatable where Case.Value: Equatable { }
 extension TextCaseCoding: Hashable where Case.Value: Hashable { }
 
-public protocol TextCase {
+protocol TextCase {
     associatedtype Value: Codable, Sendable
 
     static var transform: @Sendable (Value) -> Value { get }
 }
 
-public enum Lowercased: TextCase {
-    public static let transform: @Sendable (String) -> String = { $0.lowercased() }
+enum Lowercased: TextCase {
+    static let transform: @Sendable (String) -> String = { $0.lowercased() }
 }
 
-public enum Uppercased: TextCase {
-    public static let transform: @Sendable (String) -> String = { $0.uppercased() }
+enum Uppercased: TextCase {
+    static let transform: @Sendable (String) -> String = { $0.uppercased() }
 }
 
-public enum Capitalized: TextCase {
-    public static let transform: @Sendable (String) -> String = { $0.capitalized }
+enum Capitalized: TextCase {
+    static let transform: @Sendable (String) -> String = { $0.capitalized }
 }
 
-public enum LowercasedOptional: TextCase {
-    public static let transform: @Sendable (String?) -> String? = { $0?.lowercased() }
+enum LowercasedOptional: TextCase {
+    static let transform: @Sendable (String?) -> String? = { $0?.lowercased() }
 }
 
-public enum UppercasedOptional: TextCase {
-    public static let transform: @Sendable (String?) -> String? = { $0?.uppercased() }
+enum UppercasedOptional: TextCase {
+    static let transform: @Sendable (String?) -> String? = { $0?.uppercased() }
 }
 
-public enum CapitalizedOptional: TextCase {
-    public static let transform: @Sendable (String?) -> String? = { $0?.capitalized }
+enum CapitalizedOptional: TextCase {
+    static let transform: @Sendable (String?) -> String? = { $0?.capitalized }
 }
 
-public enum LowercasedArray: TextCase {
-    public static let transform: @Sendable ([String]) -> [String] = { $0.map { $0.lowercased() } }
+enum LowercasedArray: TextCase {
+    static let transform: @Sendable ([String]) -> [String] = { $0.map { $0.lowercased() } }
 }
 
-public enum UppercasedArray: TextCase {
-    public static let transform: @Sendable ([String]) -> [String] = { $0.map { $0.uppercased() } }
+enum UppercasedArray: TextCase {
+    static let transform: @Sendable ([String]) -> [String] = { $0.map { $0.uppercased() } }
 }
 
-public enum CapitalizedArray: TextCase {
-    public static let transform: @Sendable ([String]) -> [String] = { $0.map(\.capitalized) }
+enum CapitalizedArray: TextCase {
+    static let transform: @Sendable ([String]) -> [String] = { $0.map(\.capitalized) }
 }
